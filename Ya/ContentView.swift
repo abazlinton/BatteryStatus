e73cfcb84dca878cc3dec62cfcbbf567e54da282
Alex Bazlinton <abazlinton@gmail.com>
2021-03-17 22:31:57 +0000

    Initial Commit

---
+++ Ya/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Ya
+//
+//  Created by Alex Bazlinton on 17/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Ya/YaApp.swift
@@ -0,0 +1,17 @@
+//
+//  YaApp.swift
+//  Ya
+//
+//  Created by Alex Bazlinton on 17/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct YaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
